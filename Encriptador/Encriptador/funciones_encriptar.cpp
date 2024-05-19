afa5efd3a195932c148578a1ee33c141492d3256
AlbertEnti <albert.rocamora@enti.cat>
2024-05-19 14:50:55 +0200

    Update funciones_encriptar.cpp

--- Encriptador/Encriptador/funciones_encriptar.cpp
+++ Encriptador/Encriptador/funciones_encriptar.cpp
@@ -5,32 +5,22 @@
 using namespace std;
 void escribirMensaje();
 void recuperarMensaje();
+void añadirMensaje();
+void encriptarMensaje();
+void eliminarArchivo();
 
 
 void menuPrincipal() {
-	
-	short eleccionMenu;
-	
-	cout << "¡Bienvenido a tu encriptador personal!" << std::endl;
-	cout << "¿Que quieres hacer?" << std::endl;
-	cout << "1. Escribir mensajes / 2. Recuperar conversacion" << std::endl;
 
-	cin >> eleccionMenu;
+	ifstream file("filename.txt");
+	file.open("filename.txt");
 
-	switch (eleccionMenu) {
-	case 1:
-		 escribirMensaje();
-		break;
-
-	case 2:
-		recuperarMensaje();	
-		break;
-
-	default:
-		cerr << "Usa uno de los dos números que se te da!!" << std::endl;
+	if (file.is_open()) {
+		recuperarMensaje();
 	}
+	else
+		escribirMensaje();
 }
-
 
 void escribirMensaje() {
 
@@ -38,37 +28,70 @@ void escribirMensaje() {
 	ofstream escribirMensaje("filename.txt");
 
 	if (!escribirMensaje.is_open()) {
-			cout << "No se pudo abrir el archivo." << std::endl;
-			escribirMensaje.close();
-	}
-		cout << "Escribe lo que quieras" << std::endl;
-		while (palabras != "exit")
-		{
-			cin >> palabras;
-			escribirMensaje << palabras;
-			cout << "Sigue escribiendo, para salir escribe: exit" << std::endl;
-		}
+		cout << "No se pudo abrir el archivo." << std::endl;
 		escribirMensaje.close();
+	}
+	cout << "Escribe lo que quieras" << std::endl;
 
+	while (palabras != "exit")
+	{
+		cin >> palabras;
+		escribirMensaje << palabras;
+		cout << "Sigue escribiendo, para salir escribe: exit" << std::endl;
+	}
+	escribirMensaje.close();
+}
+
+void recuperarMensaje() {
+	
+	string palabras;
+	string textoRecuperacion;
+	short opcionAñadir;
+	
+	cout << "Mensajes guardados detectados, ¿desea recuperarlos? " << endl;
+	cout << "1. Si / 2. No, quiero machacar el archivo " << endl;
+
+	cin >> opcionAñadir;
+
+		switch (opcionAñadir) {
+		
+		case 1:
+			añadirMensaje();
+			break;
+		case 2:
+			eliminarArchivo();
+			break;
+		default:
+			cout << "Usa los números que se te dan!!!" << endl;
+			break;
+		}
+}
+
+void encriptarMensaje() {
 
 }
 
+void añadirMensaje() {
 
-void recuperarMensaje() {
-	short opcionAñadir;
 	string textoRecuperacion;
-	ifstream recuperarMensaje("filename.txt");
-	while (getline (recuperarMensaje, textoRecuperacion)) {
-		cout << textoRecuperacion;
+	string palabrasAñadir;
+	ofstream recuperarMensaje_2("filename.txt", std::ios::app);
+
+	while (getline(recuperarMensaje_2, textoRecuperacion)) {
+		cout << textoRecuperacion << endl;
 	}
-	/*cout << "Quieres añadir nuevos mensajes?" << endl;
-		cin >> opcionAñadir;
-	*/
-	recuperarMensaje.close();
 
+	cout << "Escribe lo que quieras" << std::endl;
 
-} 
+	while (palabrasAñadir != "exit") {
+		cin >> palabrasAñadir;
+		recuperarMensaje_2 << palabrasAñadir;
+		cout << "Sigue escribiendo, para salir escribe: exit" << std::endl;
+	}
+}
 
-
-
-
+void eliminarArchivo() {
+	remove("filename.txt");
+	cout << "Archivo eliminado, saliendo del programa..." << endl;
+	exit(0);
+}
